889c3eff1192e34501e2121e2e317e8c5ea7b251
renatollopezz <renatollopezzdev@gmail.com>
2017-02-02 22:16:15 -0400

    add migrations: City,State and Students

---
+++ back-end/database/migrations/2017_02_03_001345_create_cities_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->string('name')->unique();
+
+            $table->timestamps();
+        });
+       
+        Schema::table('cities', function($table){
+            $table->dropPrimary('cities_id_primary');
+        });
+
+        Schema::table('cities', function($table){
+            $table->primary(array('id', 'state_id'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
