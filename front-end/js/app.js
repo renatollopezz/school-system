b1afe32172850e7cccd286a8b9414348e179d88e
renatollopezz <renatollopezzdev@gmail.com>
2017-02-27 15:08:05 -0400

    add estrutura projeto front,packge.json e criação do arquivo de configuracao do angular

---
+++ front-end/js/app.js
@@ -0,0 +1,13 @@
+//name and injections to project
+var app = angular.module('app',['ngRoute','ngResource']);
+
+//router defines
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/',{templateURL:"index.html"})
+	}
+]);
+
+//controllers define
+
+//services define
